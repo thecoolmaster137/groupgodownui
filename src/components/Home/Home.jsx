b1f4fa06a694f9271525b21e284de685244ff22e
Ghevaram Suthar <133560659+ghevaram@users.noreply.github.com>
2023-12-06 16:39:24 +0530

    updated addgroup page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -33,6 +33,52 @@ export default function Home() {
         </div>
         <button className="btn btn-primary">Find Group</button>
       </div>
+
+      <div className="card">
+        <div className="card-body">
+          <img
+            src="https://png.pngtree.com/png-clipart/20191121/original/pngtree-group-icon-png-image_5097424.jpg"
+            width={"8%"}
+          />
+          <div className="heading-div">
+            <h5>
+              <a href="#">Group Heading</a>
+            </h5>
+            <div>
+              <a href="#">
+                <i class="bi bi-list"></i> Type
+              </a>
+              <a href="#">
+                <i class="bi bi-globe"></i> Location
+              </a>
+              <a href="#">
+                <i class="bi bi-translate"></i> Language
+              </a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="text-center">
+            <a href="#"> Group Description</a>
+          </p>
+          <hr />
+        </div>
+        <div className="ps-4 pe-4 d-flex justify-content-between">
+          <a href="#">Join Group </a>
+          <p>
+            Share on :{" "}
+            <a href="#">
+              <i class="bi bi-whatsapp"></i>
+            </a>
+            <a href="#">
+              <i class="bi bi-instagram"></i>
+            </a>
+            <a href="#">
+              <i class="bi bi-telegram"></i>
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
