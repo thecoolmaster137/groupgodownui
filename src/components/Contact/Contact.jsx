e36827e368c72ff7e9cc978b6b3b51357fe3779b
Ghevaram Suthar <133560659+ghevaram@users.noreply.github.com>
2023-12-15 15:58:31 +0530

    added the contact, privacy, terms pages

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,34 @@
 import React from "react";
-
+import "./Contact.css";
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-outer-div">
+      <div className="contact-main-div">
+        <h5>Contact Us</h5>
+        <p>Email : glinks@groups.com</p>
+        <div class="mb-3">
+          <input type="text" class="form-control" placeholder="Your Name" />
+        </div>
+        <div class="mb-3">
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Your Email"
+          />
+        </div>
+        <div class="mb-3">
+          <input type="text" class="form-control" placeholder="Subject" />
+        </div>
+        <div class="mb-3">
+          <textarea
+            class="form-control"
+            rows="3"
+            placeholder="Your Message"
+          ></textarea>
+        </div>
+        <button className="btn btn-success">Submit</button>
+      </div>
+    </div>
+  );
 }
--- src/components/Terms/Terms.jsx
+++ src/components/Terms/Terms.jsx
@@ -1,5 +1,345 @@
 import React from "react";
-
+import "./Terms.css";
 export default function Terms() {
-  return <div>Terms</div>;
+  return (
+    <div className="outer-div">
+      <div className="terms-main-div">
+        <h5>Terms of Service</h5>
+        <p>
+          We are not associated with the WhatsApp Messenger in any means.
+          WhatsApp is a registered trademark of WhatsApp Inc. & www.whastapp.com
+        </p>
+        <h5>Acceptance of Terms</h5>
+        <p>
+          By accessing, linking to or using this website you confirm your
+          acceptance of these Terms and Conditions, which may be updated by us
+          from time to time with or without notice to you. Your access or use of
+          the services in this website signifies that you agree to become bound
+          by these Terms and Conditions. If you do not agree to all the Terms,
+          then you are not permitted to access or use the content or any service
+          in the website. While joining the group you agree that any
+          conversation in the group or carried out personally is through your
+          sole discretion and Groupsor does not hold any responsibility for the
+          conversation or any outcome of the conversation.
+        </p>
+        <h5>Description of Service</h5>
+        <p>
+          Groupsor currently provides users with certain services and content,
+          as described more fully on the site, (the "Services"). You also
+          understand and agree that Groupsor assumes no responsibility for the
+          suitability, reliability, availability, timeliness and accuracy of the
+          information and services contained within Groupsor website for any
+          purpose. Services are provided "AS IS" without warranty of any kind.
+        </p>
+        <h5>Modification of Service</h5>
+        <p>
+          Groupsor reserves the right at any time and from time to time to
+          modify or discontinue, temporarily or permanently, the Service (or any
+          part thereof) with or without notice to you. You agree that Groupsor
+          shall not be liable to you or to any third party for any modification,
+          suspension or discontinuance of the Service.
+        </p>
+        <h5>Registration Obligations</h5>
+        <p>
+          You are not required to register with Groupsor before using the
+          services, but you understand that certain features of the service may
+          not be accessible to you unless you register. In registering for the
+          service you agree to provide Groupsor with true, accurate and complete
+          registration information. Groupsor reserves the right to suspend or
+          terminate your account and refuse any and all current or future use of
+          the Service (or any portion thereof), with or without notice to you.
+          Services are available only to individuals who are atleast 13 years
+          old. Any use of or access to the website by anyone under 13 years of
+          age is unauthorized and in violation of these Terms and conditions.
+        </p>
+        <h5>Privacy policy</h5>
+        <p>You may review Groupsor privacy policy here: Privacy Policy</p>
+        <h5>User Conduct</h5>
+        <p>
+          You understand that all information, data, text, software, music,
+          sound, photographs, graphics, video, messages or other materials
+          ("Content"), whether publicly posted or privately transmitted, are the
+          sole responsibility of the person from which such Content originated.
+          This means that you, and not Groupsor, are entirely responsible for
+          all Content that you upload, post, email, transmit or otherwise make
+          available via the Service. Groupsor does not control the Content
+          posted via the Service and, as such, does not guarantee the accuracy,
+          integrity or quality of such Content. You understand that by using the
+          Service, you may be exposed to Content that is offensive, indecent or
+          objectionable. Under no circumstances will Groupsor be liable in any
+          way for any Content, including, but not limited to, for any errors or
+          omissions in any Content, or for any loss or damage of any kind
+          incurred as a result of the use of any Content posted, emailed,
+          transmitted or otherwise made available via the Service. You agree to
+          not use the Service to:
+        </p>
+        <p>
+          (a) link to, upload, post, email, transmit or otherwise make available
+          any Content that is adult in nature, unlawful, harmful, threatening,
+          abusive, harassing, tortious, defamatory, vulgar, obscene, libelous,
+          invasive of another's privacy, hateful, or racially, ethnically or
+          otherwise objectionable;
+        </p>
+        <p>(b) harm minors in any way;</p>
+        <p>
+          (c) impersonate any person or entity, including, but not limited to, a
+          Groupsor official, guide or host, or falsely state or otherwise
+          misrepresent your affiliation with a person or entity;
+        </p>
+        <p>
+          (d) forge headers or otherwise manipulate identifiers in order to
+          disguise the origin of any content transmitted through the Service;
+        </p>
+        <p>
+          (e) link, upload, post, email, transmit or otherwise make available
+          any content that you do not have a right to make available under any
+          law or under contractual or fiduciary relationships (such as inside
+          information, proprietary and confidential information learned or
+          disclosed as part of employment relationships or under nondisclosure
+          agreements);
+        </p>
+        <p>
+          (f) link, upload, post, email, transmit or otherwise make available
+          any content that infringes any patent, trademark, trade secret,
+          copyright or other proprietary rights ("Rights") of any party;
+        </p>
+        <p>
+          (g) link, upload, post, email, transmit or otherwise make available
+          any unsolicited or unauthorized advertising, promotional materials,
+          "junk mail," "spam," "chain letters," "pyramid schemes," or any other
+          form of solicitation, except in those areas (such as shopping rooms)
+          that are designated for such purpose (please read our complete Spam
+          Policy);
+        </p>
+        <p>
+          (h) link, upload, post, email, transmit or otherwise make available
+          any material that contains software viruses or any other computer
+          code, files or programs designed to interrupt, destroy or limit the
+          functionality of any computer software or hardware or
+          telecommunications equipment;
+        </p>
+        <p>
+          (i) disrupt the normal flow of dialogue, cause a screen to "scroll"
+          faster than other users of the Service are able to type, or otherwise
+          act in a manner that negatively affects other users' ability to engage
+          in real time exchanges;
+        </p>
+        <p>
+          (j) interfere with or disrupt the Service or servers or networks
+          connected to the Service, or disobey any requirements, procedures,
+          policies or regulations of networks connected to the Service;
+        </p>
+        <p>
+          (k) intentionally or unintentionally violate any applicable local,
+          state, national or international law, including, but not limited to,
+          regulations promulgated by the U.S. Securities and Exchange
+          Commission, any rules of any national or other securities exchange,
+          including, without limitation, the New York Stock Exchange, the
+          American Stock Exchange or the NASDAQ, and any regulations having the
+          force of law;
+        </p>
+        <p>(l) "stalk" or otherwise harass another; or</p>
+        <p>(m) collect or store personal data about other users.</p>
+        <p>
+          (n) attempt to falsify traffic in order to earn revenue, not limited
+          to using automated 'computer' traffic, disguising traffic through
+          anonymous proxy servers, 'hiding' the Service through any method.
+        </p>
+        <p>
+          (o) register more than one account per person, accept when one is a
+          Publisher account and one is an Advertiser account. If a publisher is
+          found to have referred themselves, the referral commission will be
+          forfeited.
+        </p>
+        <p>
+          (p) offer any form of incentive to users who click on an Groupsor
+          link, not limited to points/bonus/money/gifts/traffic or link
+          exchange. You acknowledge that Groupsor may OR MAY NOT pre-screen
+          Content, but that Groupsor shall have the right (but not the
+          obligation) in their sole discretion to PRE-SCREEN, refuse, or move
+          any Content (or Account) that is available via the Service. Without
+          limiting the foregoing, Groupsor and its designees shall have the
+          right to remove any Content or Accounts that violates the TOS or is
+          otherwise objectionable. You agree that you must evaluate, and bear
+          all risks associated with, the use of any Content or Accounts,
+          including any reliance on the accuracy, completeness, or usefulness of
+          such Content. In this regard, you acknowledge that you may not rely on
+          any Content or Accounts created by Groupsor or submitted to Groupsor,
+          including without limitation information in Groupsor message boards,
+          and in all other parts of the Service. You acknowledge, consent and
+          agree that Groupsor may access, preserve, and disclose your account
+          information and Content if required to do so by law or in a good faith
+          belief that such access preservation or disclosure is reasonably
+          necessary to:
+        </p>
+        <p>(a) comply with legal process;</p>
+        <p>(b) enforce the TOS;</p>
+        <p>
+          (c) respond to claims that any Content violates the rights of
+          third-parties;
+        </p>
+        <p>(d) respond to your requests for customer service; or</p>
+        <p>
+          (e) protect the rights, property, or personal safety of Groupsor, its
+          users and the public. You understand that the technical processing and
+          transmission of the Service, including your Content, may involve
+        </p>
+        <p>(a) transmissions over various networks; and</p>
+        <p>
+          (b) changes to conform and adapt to technical requirements of
+          connecting networks or devices. You understand that the Service and
+          software embodied within the Service may include security components
+          that permit digital materials to be protected, and use of these
+          materials is subject to usage rules set by Groupsor and/or content
+          providers who provide content to the Service. You may not attempt to
+          override or circumvent any of the usage rules embedded into the
+          Service. Any unauthorized reproduction, publication, further
+          distribution or public exhibition of the materials provided on the
+          Service, in whole or in part, is strictly prohibited.
+        </p>
+        <h5>Submitted Content</h5>
+        <p>
+          Groupsor does not claim ownership of content or links you submit or
+          make available for inclusion on the Service. However, with respect to
+          content or links you submit or make available for inclusion on the
+          Service, you grant Groupsor the following world-wide, royalty free and
+          non-exclusive license(s) with respect to content or link you submit or
+          make available for inclusion on Groupsor, the license to use,
+          distribute, reproduce, modify, adapt, publicly perform and publicly
+          display such content or link on the Service solely for the purposes of
+          providing and promoting the specific Service which such content or
+          link was submitted or made available. This license exists only for as
+          long as you elect to continue to include such content or link on the
+          Service and will terminate at the time you remove or Groupsor removes
+          such content or link from the Service.
+        </p>
+        <h5>User created Groups</h5>
+        <p>
+          Groupsor does not claim ownership of content or links that are
+          submitted in the user created groups. Groupsor shall not be liable for
+          the conversation or content or any other media communicated in these
+          groups.
+        </p>
+        <h5>Links and References</h5>
+        <p>
+          The Service may contain links to third-party websites or resources.
+          Because Groupsor has no control over such websites and resources, you
+          acknowledge and agree that Groupsor is not responsible or liable for:
+        </p>
+        <p>
+          (a) the availability or accuracy of such websites or resources; or
+        </p>
+        <p>
+          (b) the content, advertising, products or services on or available
+          from such websites or resources. You further acknowledge and agree
+          that Groupsor shall not be responsible or liable, directly or
+          indirectly, for any damage or loss caused or alleged to be caused by
+          or in connection with use of or reliance on any such content, goods or
+          services available on or through any such website or resource.
+        </p>
+        <h5>Termination</h5>
+        <p>
+          You agree that Groupsor, in its sole discretion, may terminate, change
+          or limit any of the Services or their availability to you, notice at
+          any time and without prior notice, with or without cause. Further, you
+          agree that Groupsor shall not be liable to you or any third-party for
+          any termination of your account, any associated email address, or
+          access to the service.
+        </p>
+        <h5>Indeminity</h5>
+        <p>
+          You agree to indemnify and hold harmless Groupsor, its parents,
+          subsidiaries, afiliates, officers, agents, partners and employees from
+          any losses, expenses, damages and costs, including reasonable
+          attorney's fees, made by any third party due to or arising out of
+          Content you submit, post, transmit or make available through the
+          Service, your use of the Service, your connection to the Service, your
+          violation of the Terms of use or your violation of any rights of
+          another.
+        </p>
+        <h5>Disclaimer of Warranties</h5>
+        <p>
+          (a) YOU AGREE THAT YOUR USE OF THE SERVICES IS AT YOUR SOLE RISK AND
+          ACKNOWLEDGE THAT THE SERVICES, INCLUDING, BUT NOT LIMITED TO, CONTENT,
+          SERVICES, GOOD, OR ADVERTISEMENT ARE PROVIDED "AS IS" AND "AS
+          AVAILABLE" BASIS. Groupsor DISCLAIMS ALL WARRANTIES OF ANY KIND,
+          WHETHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED
+          WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, TITLE, OR FITNESS FOR
+          A PARTICULAR PURPOSE OR USE.
+        </p>
+        <p>
+          (b) Groupsor MAKES NO WARRANTY THAT (i) THE SERVICE WILL MEET YOUR
+          REQUIREMENTS, (ii) THE SERVICE WILL BE UNINTERRUPTED, TIMELY, SECURE,
+          OR ERROR-FREE, (iii) THE RESULTS THAT MAY BE OBTAINED FROM THE USE OF
+          THE SERVICE WILL BE ACCURATE OR RELIABLE, (iv) THE QUALITY OF ANY
+          PRODUCTS, SERVICES, INFORMATION, OR OTHER MATERIAL PURCHASED OR
+          OBTAINED BY YOU THROUGH THE SERVICE WILL MEET YOUR EXPECTATIONS, AND
+          (V) ANY ERRORS IN THE SOFTWARE WILL BE CORRECTED.
+        </p>
+        <p>
+          (c) ANY MATERIAL DOWNLOADED OR OTHERWISE OBTAINED THROUGH THE USE OF
+          THE SERVICE IS DONE AT YOUR OWN DISCRETION AND RISK AND THAT YOU WILL
+          BE SOLELY RESPONSIBLE FOR ANY DAMAGE TO YOUR COMPUTER SYSTEM OR LOSS
+          OF DATA THAT RESULTS FROM THE DOWNLOAD OF ANY SUCH MATERIAL.
+        </p>
+        <p>
+          (d) NO ADVICE OR INFORMATION, WHETHER ORAL OR WRITTEN, OBTAINED BY YOU
+          FROM Groupsor OR THROUGH OR FROM THE SERVICE SHALL CREATE ANY WARRANTY
+          NOT EXPRESSLY STATED IN THE TOS.
+        </p>
+        <p>
+          (e) A SMALL PERCENTAGE OF USERS MAY EXPERIENCE EPILEPTIC SEIZURES WHEN
+          EXPOSED TO CERTAIN LIGHT PATTERNS OR BACKGROUNDS ON A COMPUTER SCREEN
+          OR WHILE USING THE SERVICE. CERTAIN CONDITIONS MAY INDUCE PREVIOUSLY
+          UNDETECTED EPILEPTIC SYMPTOMS EVEN IN USERS WHO HAVE NO HISTORY OF
+          PRIOR SEIZURES OR EPILEPSY. IF YOU, OR ANYONE IN YOUR FAMILY, HAVE AN
+          EPILEPTIC CONDITION, CONSULT YOUR PHYSICIAN PRIOR TO USING THE
+          SERVICE. IMMEDIATELY DISCONTINUE USE OF THE SERVICE AND CONSULT YOUR
+          PHYSICIAN IF YOU EXPERIENCE ANY OF THE FOLLOWING SYMPTOMS WHILE USING
+          THE SERVICE -- DIZZINESS, ALTERED VISION, EYE OR MUSCLE TWITCHES, LOSS
+          OF AWARENESS, DISORIENTATION, ANY INVOLUNTARY MOVEMENT, OR
+          CONVULSIONS.
+        </p>
+        <h5>Limitation of Liabilities</h5>
+        <p>
+          (a) YOU UNDERSTAND AND AGREE THAT Groupsor AND ITS SUBSIDIARIES,
+          AFFILIATES, OFFICERS, EMPLOYEES, AGENTS, PARTNERS AND LICENSORS SHALL
+          NOT BE LIABLE TO YOU FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+          CONSEQUENTIAL OR EXEMPLARY DAMAGES, INCLUDING, BUT NOT LIMITED TO,
+          DAMAGES FOR LOSS OF PROFITS, GOODWILL, USE, DATA OR OTHER INTANGIBLE
+          LOSSES (EVEN IF Groupsor HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+          DAMAGES), RESULTING FROM: (i) YOUR ACCESS TO OR USE OF OR INABILITY TO
+          ACCESS OR USE THE SERVICES; (ii) THE COST OF PROCUREMENT OF SUBSTITUTE
+          GOODS OR SERVICES RESULTING FROM ANY GOODS, DATA, INFORMATION OR
+          SERVICES PURCHASED OR OBTAINED OR MESSAGES RECEIVED OR TRANSACTIONS
+          ENTERED INTO THROUGH OR FROM THE SERVICE; (iii) UNAUTHORIZED ACCESS TO
+          OR ALTERATION OF YOUR TRANSMISSIONS OR DATA; (iv) STATEMENTS OR
+          CONDUCT OF ANY THIRD PARTY ON THE SERVICE; OR (v) ANY OTHER MATTER
+          RELATING TO THE SERVICE.
+        </p>
+        <p>
+          (b) YOU UNDERSTAND AND AGREE THAT BY USING THE SERVICE, YOU MAY BE
+          EXPOSED TO CONTENT THAT IS OFFENSIVE, INDECENT OR OBJECTIONABLE. UNDER
+          NO CIRCUMSTANCES WILL Groupsor AND ITS SUBSIDIARIES, AFFILIATES,
+          OFFICERS, EMPLOYEES, AGENTS, PARTNERS AND LICENSORS BE LIABLE IN ANY
+          WAY FOR ANY CONTENT, INCLUDING, BUT NOT LIMITED TO, ANY ERRORS OR
+          OMISSIONS IN ANY CONTENT, OR ANY LOSS OR DAMAGE OF ANY KIND INCURRED
+          AS A RESULT OF THE USE OF ANY CONTENT POSTED, E-MAILED, TRANSMITTED OR
+          OTHERWISE MADE AVAILABLE VIA THE SERVICE.
+        </p>
+        <p>
+          (c) YOU UNDERSTAND AND AGREE THAT THE SERVICE IS PROVIDED "AS-IS" AND
+          THAT Groupsor AND ITS SUBSIDIARIES, AFFILIATES, OFFICERS, EMPLOYEES,
+          AGENTS, PARTNERS AND LICENSORS ASSUME NO RESPONSIBILITY FOR THE
+          TIMELINESS, DELETION, MIS-DELIVERY OR FAILURE TO STORE ANY USER
+          COMMUNICATIONS OR PERSONALIZATION SETTINGS.
+        </p>
+        <h5>Violations</h5>
+        <p>
+          Please report any violations of these Terms & Conditions by using our
+          contact form: Contact
+        </p>
+      </div>
+    </div>
+  );
 }
